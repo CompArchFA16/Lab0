301fb862aabfe91b8e9aa2a6a68c5678a2909ceb
Ian Paul <ian.paul@students.olin.edu>
2016-09-23 19:41:36 +0000

    added the adder with the full adder 4bit module

---
+++ adder.v
@@ -0,0 +1,50 @@
+// Adder circuit
+
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+`define XOR xor #50
+
+module behavioralFullAdder
+(
+    output sum, 
+    output carryout,
+    input a, 
+    input b, 
+    input carryin
+);
+    // Uses concatenation operator and built-in '+'
+    assign {carryout, sum}=a+b+carryin;
+endmodule
+
+module structuralFullAdder
+(
+    output sum, 
+    output carryout,
+    input a, 
+    input b, 
+    input carryin
+);
+    wire AandB, AandC, BandC;
+
+    `XOR xorGate(sum, a, b, carryin);
+
+    `AND andGate0(AandB, a, b);
+    `AND andGate1(AandC, a, carryin);
+    `AND andGate2(BandC, b, carryin);
+
+    `OR orGate(carryout, AandB, AandC, BandC);
+
+endmodule
+
+
+module FullAdder4bit
+(
+  output[3:0] sum,  // 2's complement sum of a and b
+  output carryout,  // Carry out of the summation of a and b
+  output overflow,  // True if the calculation resulted in an overflow
+  input[3:0] a,     // First operand in 2's complement format
+  input[3:0] b      // Second operand in 2's complement format
+);
+    // Your Code Here
+endmodule
