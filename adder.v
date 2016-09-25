461fa710e5eedd1c212d26267b4df9da2c6e5693
Jee Hyun Kim <jeehyun.kim@students.olin.edu>
2016-09-25 12:01:34 -0400

    eh room meeting

--- adder.v
+++ adder.v
@@ -40,17 +40,18 @@ endmodule
 
 module FullAdder4bit
 (
-  output[3:0] sum,  // 2's complement sum of a and b
+  output[4:0] sum,  // 2's complement sum of a and b
   output carryout,  // Carry out of the summation of a and b
   output overflow,  // True if the calculation resulted in an overflow
-  input[3:0] a,     // First operand in 2's complement format
-  input[3:0] b      // Second operand in 2's complement format
+  input[4:0] a,     // First operand in 2's complement format
+  input[4:0] b      // Second operand in 2's complement format
 );
+
     wire temp_cout[2:0];
 
     structuralFullAdder fa0(sum[0], temp_cout[0], a[0], b[0], 0);
     structuralFullAdder fa1(sum[1], temp_cout[1], a[1], b[1], temp_cout[0]);
     structuralFullAdder fa2(sum[2], temp_cout[2], a[2], b[2], temp_cout[1]);
     structuralFullAdder fa3(sum[3], carryout, a[3], b[3], temp_cout[2]);
-    assign overflow = carryout^temp_cout[2];
+    `XOR xorGate(overflow, carryout, temp_cout[2]);
 endmodule
--- fulladder.t.v
+++ fulladder.t.v
@@ -4,24 +4,24 @@
 
 module testFullAdder4bit ();
 
-    wire [3:0] sum;
-    reg [3:0] a, b;
-    wire cout, overflow;
+    wire [4:0] sum;
+    reg [4:0] a, b;
+    wire carryout, overflow;
 
-    FullAdder4bit fa4b(sum, cout, overflow, a, b);
+    FullAdder4bit fa4b(sum, carryout, overflow, a, b);
 
 	initial begin
 
-        // Dump trace to a file. Open with gtkwave.
-        //$dumpfile("multiplexer_trace.vcd");
-        //$dumpvars();
-        //$dumpall;
+        //Dump trace to a file. Open with gtkwave.
+        $dumpfile("fulladder4bit.vcd");
+        $dumpvars();
+        $dumpall;
 
     	$display("  a    b  |overflow carryout   sum ");
-    	for(a=3'b000; a<=3'b111; a=a+1) begin
-            for (b=3'b000; b<=3'b111; b=b+1) begin
+    	for(a=4'b0000; a<=4'b1111; a=a+1) begin
+            for (b=4'b0000; b<=4'b1111; b=b+1) begin
             	#1000
-            	$display("%b%b%b%b %b%b%b%b |   %b       %b      %b%b%b%b", a[3], a[2], a[1], a[0], b[3], b[2], b[1], b[0], overflow, cout, sum[3], sum[2], sum[1], sum[0]);
+            	$display("%b%b%b%b %b%b%b%b |   %b        %b       %b%b%b%b", a[3], a[2], a[1], a[0], b[3], b[2], b[1], b[0], overflow, carryout, sum[3], sum[2], sum[1], sum[0]);
             end
         end
     end
