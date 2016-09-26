ef65b1085c0bedfdcd20d39738909c29cd239c8a
David Zhu <hdavidzhu@gmail.com>
2016-09-26 11:49:17 -0400

    ADD: More expects.

--- adder4bit.t.v
+++ adder4bit.t.v
@@ -11,20 +11,20 @@ module testAdder4bit();
     initial begin
     $dumpfile("fullAdder4bit.vcd");
     $dumpvars;
-    
-    $display("A     B      | Sum  CarryOut  Overflow | Expected Output");
+
+    $display("A     B      | Sum   CarryOut Overflow | ESum ECarryout EOverflow");
     a[3]=0;a[2]=1;a[1]=0;a[0]=0;b[3]=0;b[2]=0;b[1]=1;b[0]=0; #1000
-    $display("%b  %b   | %b  %b  %b | 0110", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 0110 ", a, b, sum, carryout, overflow);
     a[0]=0;a[1]=0;a[2]=1;a[3]=0;b[0]=1;b[1]=1;b[2]=0;b[3]=1; #1000
-    $display("%b  %b   | %b  %b  %b | 1111", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 1111 ", a, b, sum, carryout, overflow);
     a[0]=1;a[1]=1;a[2]=1;a[3]=0;b[0]=0;b[1]=0;b[2]=1;b[3]=1; #1000
-    $display("%b  %b   | %b  %b  %b | 0001", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 0001 ", a, b, sum, carryout, overflow);
     a[0]=1;a[1]=0;a[2]=1;a[3]=1;b[0]=1;b[1]=1;b[2]=1;b[3]=1; #1000
-    $display("%b  %b   | %b  %b  %b | 1100", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 1100 ", a, b, sum, carryout, overflow);
     a[0]=0;a[1]=1;a[2]=0;a[3]=1;b[0]=0;b[1]=1;b[2]=0;b[3]=0; #1000
-    $display("%b  %b   | %b  %b  %b | 1001", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 1001 ", a, b, sum, carryout, overflow);
     a[0]=1;a[1]=0;a[2]=0;a[3]=0;b[0]=1;b[1]=1;b[2]=1;b[3]=0; #1000
-    $display("%b  %b   | %b  %b  %b | 0110", a, b, sum, carryout, overflow);
+    $display("%b  %b   | %b  %b        %b        | 0110 ", a, b, sum, carryout, overflow);
 
     end
 endmodule
