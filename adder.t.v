0f54c7f31e5bb49626f5f25ff86bfffd864d602c
William Lu <william.a.lu@gmail.com>
2016-09-27 22:06:58 -0400

    added adder test bench with 16 test cases

--- adder.t.v
+++ adder.t.v
@@ -15,8 +15,53 @@ module testFullAdder4bit();
     // $dumpfile("testFullAdder4bit.vcd");
     // $dumpvars(0, testFullAdder4bit);
 
-    opA =4;opB=4; #1000;
-    $display(" %b  %b  |  %b  %b  %b", opA, opB, res0, cout, ovf);
+    $display("  A     B    |   Sum  Co  Ovf | Expected Output");
+
+    //No carryout, no overflow
+    opA=0; opB=0; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0000 0 0", opA, opB, res0, cout, ovf);
+    opA=1; opB=1; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0010 0 0", opA, opB, res0, cout, ovf);
+    opA=3; opB=4; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0111 0 0", opA, opB, res0, cout, ovf);
+    opA=1; opB=-7; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1010 0 0", opA, opB, res0, cout, ovf);
+
+    $display("-----------------------------------------------");
+
+    //Carryout, no overflow
+    opA=-4; opB=-4; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1000 1 0", opA, opB, res0, cout, ovf);
+    opA=-1; opB=-1; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1110 1 0", opA, opB, res0, cout, ovf);
+    opA=6; opB=-1; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0101 1 0", opA, opB, res0, cout, ovf);
+    opA=4; opB=-4; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0000 1 0", opA, opB, res0, cout, ovf);
+
+    $display("-----------------------------------------------");
+
+    //No carryout, overflow
+    opA=4; opB=4; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1000 0 1", opA, opB, res0, cout, ovf);
+    opA=7; opB=7; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1110 0 1", opA, opB, res0, cout, ovf);
+    opA=6; opB=7; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1101 0 1", opA, opB, res0, cout, ovf);
+    opA=5; opB=6; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 1011 0 1", opA, opB, res0, cout, ovf);
+
+    $display("-----------------------------------------------");
+
+    //Carryout, overflow
+    opA=-8; opB=-8; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0000 1 1", opA, opB, res0, cout, ovf);
+    opA=-8; opB=-1; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0111 1 1", opA, opB, res0, cout, ovf);
+    opA=-6; opB=-7; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0011 1 1", opA, opB, res0, cout, ovf);
+    opA=-5; opB=-6; #1000;
+    $display(" %b  %b  |  %b  %b    %b  | 0101 1 1", opA, opB, res0, cout, ovf);
 
     // for (i = 0; i < 16; i = i +1) begin
     //   $display ("Current value of i is %d", i);
