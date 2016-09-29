4e1adad86bb4ec15faeeaf44c4760e9f53514729
DHZBill <haozheng.du@students.olin.edu>
2016-09-28 23:33:24 -0400

    implemented signed 4-bit adder with test cases

--- adder.t.v
+++ adder.t.v
@@ -2,6 +2,10 @@
 `timescale 1 ns / 1 ps
 `include "adder.v"
 
+`define TEST(a_v,b_v) a=a_v; b=b_v; #1000;
+
+`define DISP(exp, msg) $display("%d %d | %d  |     %d      |    %d     |%d : %s",  a, b, sum, a + b, overflow, exp, msg);
+
 module testFullAdder4bit();
 	reg signed [4:0] a;
 	reg signed [4:0] b;
@@ -14,17 +18,41 @@ module testFullAdder4bit();
 
     	$dumpfile("FullAdder4bit.vcd");
 		$dumpvars(0, testFullAdder4bit);
-		
-		$display("Testing : ");
-		$display("a  b | sum | expected sum | overflow | expected overflow");
-		a = 14; b = 14; #1000
-		$display("%d %d | %d |      %d      |    %d     |%d", a, b, sum, a + b, overflow, 1);
 
-		$display("Testing : ");
-		$display("a  b | sum | expected sum | overflow | expected overflow");
-		a = 2; b = 8; #1000
-		$display("%d %d | %d |      %d      |    %d     |%d", a, b, sum, a + b, overflow, 0);
-		
+		$display("  a   b | sum | expected sum | overflow | expected overflow");
+		`TEST(0,0); 
+		`DISP(0,"Adding two zeros");
+		`TEST(1,2); 
+		`DISP(0,"Positive no overflow");
+		`TEST(7,3); 
+		`DISP(1,"Positive overflow");
+		`TEST(7,1); 
+		`DISP(1,"Positive boundary overflow");
+		`TEST(-1,-3); 
+		`DISP(0,"Negative no overflow");
+		`TEST(-5,-6); 
+		`DISP(1,"Negative overflow");
+		`TEST(-4,-4); 
+		`DISP(0,"Negative boundary no overflow");
+		`TEST(-3,-6); 
+		`DISP(1,"Negative boundary overflow");
+		`TEST(7,7); 
+		`DISP(1,"Positive extreme");
+		`TEST(-8,-8); 
+		`DISP(1,"Negative extreme");
+		`TEST(2,-2); 
+		`DISP(0,"Cancel out");
+		`TEST(-1,-1); 
+		`DISP(0,"Signed VS Unsigned");
+		`TEST(2,1); 
+		`DISP(0,"Symmetry");
+		`TEST(3,0); 
+		`DISP(0,"Positive Equality");
+		`TEST(-4,0); 
+		`DISP(0,"Negative Equality");
+		`TEST(-3,7); 
+		`DISP(0,"Random test");
+
 		$display(" a  b | sum | overflow | carryout");
 		
 		for(a= -8; a < 8; a = a + 1) begin
